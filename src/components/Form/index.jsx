a56257d629f5ac2fa7dcb2561cc69c2e5e906e11
Renan Mattar <renanmdr3@gmail.com>
2023-05-28 17:43:17 -0300

    Criando o Form e adicionando o Style com Mui

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,10 @@
+import { TextField } from "@mui/material";
+
+export default function Form(){
+
+    return(
+        <>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </>
+    )
+}
